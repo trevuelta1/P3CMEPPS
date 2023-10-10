3dcab3a36ed59b9b52b2e2859ae80abce8032569
usuario <usuario@pc21-087>
2023-10-10 16:28:17 +0200

    v0.2 (minimum test done)

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -2,4 +2,24 @@ package calculadora;
 
 public class Calculadora {
 
+	public static Integer suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	public static Integer resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return -1;
+	}
+
+	public static Integer multiplica(int i, int j) {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+	public static Integer divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
